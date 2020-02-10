29cfd228dae1aa9ac3ec488acc8788a2590f7552
studenti <studenti@rosselli.loc>
2020-02-10 09:52:39 +0100

    Add project files.

---
+++ StrutturaDati4/StruttureDati.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StrutturaDati4
+{
+    class StruttureDati
+    {
+        static void Main(string[] args)
+        {
+            string test = "test.txt";
+            Console.WriteLine("inserisci un numero:");
+            string numero = Console.ReadLine();
+            using (StreamReader sr = new StreamReader("test.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == numero)
+                    {
+                        Console.WriteLine($"il numero esiste");
+                        Console.ReadLine();
+                    }
+                    else if (line != numero)
+                    {
+                        Console.WriteLine($"il numero non esiste");
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+         
+    }
+}
